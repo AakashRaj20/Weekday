c188d2f01d158d2458fb533b8c2e487459e8e842
Aakash Raj <aakashraj914@gmail.com>
2024-05-05 22:28:25 +0530

    added padding to the main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
-import { Container,  } from '@mui/material'
+import { Box,  } from '@mui/material'
 import Filters from './components/Filters'
 import JobCards from './components/JobCards'
 
 function App() {
 
   return (
-    <>
+    <Box fluid sx={{ px: { xs: "1rem", sm: "2rem", md: "3rem", lg:"5rem" }, py: "1rem", display: "flex", flexDirection: "column", gap: "6rem" }}>
      <Filters />
      <JobCards />
-    </>
+    </Box>
   )
 }
 
